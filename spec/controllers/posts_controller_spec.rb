243988a7e513b3c0c9dfab0d04afddf358f7a528
Jeffrey King <jdking33@aol.com>
2016-10-22 12:59:32 -0700

    Add show, new, create to post resource

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,19 +15,55 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
 
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it "should render the #show view" do
+      get :show, {id: my_post.id}
+      expect(response).to render_template(:show)
+    end
+
+    it "should assign my_post to @post" do
+      get :show, {id: my_post.id}
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should render the #new view" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it "should instantiate @post" do
+      get :new
+      expect(assigns(:post)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "should increase the number of Post by 1" do
+      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+    end
+
+    it "should assign the new post to @post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_sentence}
+      expect(assigns(:post)).to eq(Post.last)
+    end
+
+    it "should redirect to the new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_sentence}
+      expect(response).to redirect_to(Post.last)
+    end
+  end
 
   # describe "GET #edit" do
   #   it "returns http success" do

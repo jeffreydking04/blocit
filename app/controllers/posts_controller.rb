451637b7869ca7bd2b995bc578b5b60b6e6087d5
Jeffrey King <jdking33@aol.com>
2016-10-22 15:18:47 -0700

    Add #edit, #update, #destroy, and related views, buttons, etc. to posts resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,5 +26,32 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved successfully."
+      redirect_to(@post)
+    else
+      flash[:alert] = "There was an error saving the post. Please attempt once more."
+      render(:edit)
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.delete
+      flash[:notice] = "\"#{@post.title}\" succesfully deleted."
+      redirect_to(posts_path)
+    else
+      flash[:alert] = "Error deleting post.  Please try again."
+      render(:show)
+    end
   end
 end

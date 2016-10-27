8446e160d716b9e9eb01c9ab844d1172f767973c
Jeffrey King <jdking33@aol.com>
2016-10-27 08:01:16 -0700

    Do Not Merge: Add name capitalize before_save callback to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  before_save { self.email =  email.downcase if email.present?}
+  before_save do
+    self.email =  email.downcase if email.present?
+    self.name = name.split.each { |x| x.capitalize! }.join(" ")
+  end
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
   validates :password, length: { minimum: 6 }, allow_blank: true
